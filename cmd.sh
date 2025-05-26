876feb1085f0cb3d23959cf5fbdd03c1869f1a36
tec <technic.robot.35@gmail.com>
2025-05-25 23:08:40 -0600

    Second revision

--- cmd.sh
+++ cmd.sh
@@ -1,5 +1,16 @@
+# Preprocessing to get csv
+python3 utils/clean_data.py data/sketch-2022-10-25.json data/sketch-2022-10-25.csv
 # Grouping each 1 months
 python3 get_lewd_ratio.py ../data/color-3-dic-21.csv --time 2018-01-01 --freq 1 --prefix graphs/nox-
+
+# new sintax
+python rating_analysis/get_lewd_ratio.py data/tom-2023-09-1-color.csv  --time-start 2015-09-01 --time-stop 2023-09-01 --freq 3 --prefix rating_analysis/graphs/2023/tom-2023-09-01-color
+
+python rating_analysis/get_lewd_ratio.py data/2024/tom-2024-01-30.csv --time-start 2015-09-01 --time-stop 2023-09-01 --freq 3 --prefix rating_analysis/graphs/2024/tom-2024-01-30
+
+
+
+
 
 
 
--- get_lewd_ratio.py
+++ get_lewd_ratio.py
@@ -10,23 +10,33 @@ six_months = mdates.MonthLocator(interval=1)
 def parse_arguments():
   parser = argparse.ArgumentParser()
   parser.add_argument('file', help = 'File To check')
-  parser.add_argument('--save', help = 'Save graphs on disk as imgs', action='store_true')  
   parser.add_argument('--prefix', help = 'Prefix name for graphs on disks')  
   parser.add_argument('--title', help = 'Prefix name for graphs on disks', default=None)  
   parser.add_argument('--freq', help = 'Group by frequency in months', default=6,type=int)  
-  parser.add_argument('--time', help = 'Only consider entries after this date')  
+  parser.add_argument('--time-start', help = 'Only consider entries after this date')  
+  parser.add_argument('--time-stop', help = 'Only consider entries after this date')  
+  parser.add_argument('--high-def',help = "Get high definition counts", action='store_true')
   return parser.parse_args()
 
-def process_file(filename,frequency=6,time_slice=None):
+def process_file(filename,time_start=None, time_stop=None):
+  """ Gets rating value s"""
+  print(f'Reading {filename}')
   df = pd.read_csv(filename)
-  if time_slice: 
-    df = df[df['creation'] > time_slice ]
-  df['creation'] = pd.to_datetime(df['creation'])
+  print(f'Got {len(df)} entries')
+  if time_start: 
+    df = df[df['creation'] > time_start ]
+  if time_stop: 
+    df = df[df['creation'] < time_stop ]
+  df['creation'] = pd.to_datetime(df['creation'], format='ISO8601')
   df['safe'] = 0
   df['questionable'] = 0
   df.loc[df['rating'] == 's','safe'] = 1
   df.loc[df['rating'] != 's','questionable'] = 1
-  
+  print("Pre-processing complete")
+  print(f'Got {len(df)} entries after prunning!')
+  return df
+
+def group_df(df,frequency=6):
   f_str = "{}MS".format(frequency)
   print("Grouping each {} months".format(frequency))
   grouper = pd.Grouper(key='creation',freq=f_str, origin='epoch',dropna=False)
@@ -40,6 +50,44 @@ def process_file(filename,frequency=6,time_slice=None):
   count_df['quest_ratio']  = count_df['questionable']/count_df['total'] * 100
   #print(count_df)
   return count_df 
+
+def group_high_def(df):
+  print("Grouping each week")
+  grouper = pd.Grouper(key='creation',freq='W', origin='epoch',dropna=False)
+  grouped_df = df.groupby(grouper)
+  count_df = grouped_df.sum().reset_index()
+  
+  # Easier than running another groupby 
+  count_df['total'] = count_df['safe'] + count_df['questionable'] 
+  count_df['ratio'] = count_df['questionable'] / count_df['safe']
+  count_df['safe_ratio'] = count_df['safe']/count_df['total'] * 100
+  count_df['quest_ratio']  = count_df['questionable']/count_df['total'] * 100
+  #print(count_df)
+  return count_df 
+
+def graph_high_def(count_df,title=None):
+  print("Graphing high definition")
+  fig,ax = plt.subplots(figsize=(12,8))
+  fig.autofmt_xdate()
+  
+  mon_l = mdates.MonthLocator(interval=1)
+  ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+  ax.xaxis.set_major_locator(mon_l)
+  
+  header = 'Number of Safe and Questionable sketches over time'
+  if title:
+    header += ':\n' + title  
+  ax.set_title(header)
+
+  ax.set_xlabel('Date')
+  ax.set_ylabel('Sketches')
+
+  ax.plot(count_df['creation'], count_df['safe'], label='Safe',color='#72CDFF')
+  ax.plot(count_df['creation'], count_df['questionable'], label='Questionable',color='#F7941D')
+
+  ax.legend()
+  return fig
+
 
 def graph_counts(count_df ,title=None,months=6):
   fig,ax = plt.subplots(figsize=(12,8))
@@ -65,7 +113,7 @@ def graph_counts(count_df ,title=None,months=6):
     header += ':\n' + title  
   ax.set_title(header)
 
-  ax.set_xlabel('Year')
+  ax.set_xlabel('Date')
   ax.set_ylabel('Sketches')
   alg='center'
   ax.bar(x_num   ,count_df['safe'],width=spacing,
@@ -75,6 +123,8 @@ def graph_counts(count_df ,title=None,months=6):
   # automfmt _xdate does this too:
   #ax.xaxis_date()
   ax.legend()
+  ax.annotate("Graph by TecBot with ❤️  ", xy= (0.8,-0.2),
+              xycoords='axes fraction', fontsize=10)
   return fig
 
 def graph_ratio(count_df,title=None,months=6):
@@ -103,6 +153,8 @@ def graph_ratio(count_df,title=None,months=6):
   ax.bar(x_num, safe_ratio, width=spacing,
          align='center',label='Safe',color='#72CDFF')
 
+  ax.annotate("Graph by TecBot with ❤️  ", xy= (0.8,-0.2),
+              xycoords='axes fraction', fontsize=10)
   ax.legend()
   return fig
 
@@ -121,17 +173,23 @@ def graph_lewd(count_df,title=None):
   ax.plot(count_df['creation'],count_df['ratio'], 
         label='Lewd Ratio', color='#72CDEE' )
 
+  ax.annotate("Graph by TecBot with ❤️  ", xy= (0.8,-0.2),
+              xycoords='axes fraction', fontsize=10)
   ax.legend()
   return fig
 
 if __name__ == '__main__':
   args = parse_arguments()
   plt.style.use('ggplot')
-  counts = process_file(args.file,args.freq,args.time)
+  df = process_file(args.file,args.time_start,args.time_stop)
+  counts = group_df(df,args.freq)
   cnt_fig = graph_counts(counts,args.title,months=args.freq)
   ratio_fig =  graph_ratio(counts,args.title,months=args.freq)
   lewd_fig =  graph_lewd(counts,args.title)
-  
+
+  #cnt_hd = group_high_def(df)
+  #fig_hd = graph_high_def(cnt_hd)
+
   if args.prefix:
     cnt_fig.savefig(args.prefix + 'sfw-nsfw-count.png')
     ratio_fig.savefig(args.prefix + 'sfw-nsfw-ratio.png')
