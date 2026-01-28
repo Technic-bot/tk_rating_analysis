395a01d49a378be4f43fe9280f5f9dd9e7300e1f
tec <technic.robot.35@gmail.com>
2026-01-27 21:37:05 -0600

    Fix date drift

--- get_lewd_ratio.py
+++ get_lewd_ratio.py
@@ -12,7 +12,7 @@ def parse_arguments():
   parser.add_argument('file', help = 'File To check')
   parser.add_argument('--prefix', help = 'Prefix name for graphs on disks')  
   parser.add_argument('--title', help = 'Prefix name for graphs on disks', default=None)  
-  parser.add_argument('--freq', help = 'Group by frequency in months', default=6,type=int)  
+  parser.add_argument('--freq', help = 'Group by frequency in months', default="QE")  
   parser.add_argument('--time-start', help = 'Only consider entries after this date')  
   parser.add_argument('--time-stop', help = 'Only consider entries after this date')  
   parser.add_argument('--high-def',help = "Get high definition counts", action='store_true')
@@ -36,10 +36,9 @@ def process_file(filename,time_start=None, time_stop=None):
   print(f'Got {len(df)} entries after prunning!')
   return df
 
-def group_df(df,frequency=6):
-  f_str = "{}MS".format(frequency)
+def group_df(df,frequency):
   print("Grouping each {} months".format(frequency))
-  grouper = pd.Grouper(key='creation',freq=f_str, origin='epoch',dropna=False)
+  grouper = pd.Grouper(key='creation',freq=frequency, origin='epoch',dropna=False)
   grouped_df = df.groupby(grouper)
   count_df = grouped_df.sum().reset_index()
   
@@ -89,21 +88,20 @@ def graph_high_def(count_df,title=None):
   return fig
 
 
-def graph_counts(count_df ,title=None,months=6):
+def graph_counts(count_df ,title=None):
   fig,ax = plt.subplots(figsize=(12,8))
-  fig.autofmt_xdate()
 
-  n_mon = 12 / months
-  spacing = 365 / n_mon / 2 
-  if months < 2 :
-    months = 2 
-  mon_l = mdates.MonthLocator(interval=months)
+  QE = 3
+  spacing=365/ (12/ QE) / 2
+  mon_l = mdates.MonthLocator(bymonth=[1,4,7,10])
   ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
   ax.xaxis.set_major_locator(mon_l)
   # To get python native datetime: 
   # py_date = count['creation'].dt.to_pydatetime()
   x_num = mdates.date2num(count_df['creation'])
+  dates = count_df['creation']
   # Again bar uses a scalar for X axis, so can't use a simple offset
+  
   # to plot multiple series
   # For the record: 
   # ax.bar(h_count['creation'],s_count['counts'],width=25)
@@ -115,27 +113,26 @@ def graph_counts(count_df ,title=None,months=6):
 
   ax.set_xlabel('Date')
   ax.set_ylabel('Sketches')
-  alg='center'
-  ax.bar(x_num   ,count_df['safe'],width=spacing,
-         align=alg,label='Safe',color='#72CDFF')
-  ax.bar(x_num + spacing,count_df['questionable'],width=spacing,
-        align=alg,label='Questionable', color='#F7941D' )
+  alg='edge'
+  ax.bar(x_num, count_df['safe'], width=spacing,
+         align=alg, label='Safe', color='#72CDFF')
+  ax.bar(x_num + spacing, count_df['questionable'], width=spacing,
+        align=alg, label='Questionable', color='#F7941D' )
   # automfmt _xdate does this too:
-  #ax.xaxis_date()
+  ax.xaxis_date()
+  plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
   ax.legend()
   ax.annotate("Graph by TecBot with ❤️  ", xy= (0.8,-0.2),
               xycoords='axes fraction', fontsize=10)
   return fig
 
-def graph_ratio(count_df,title=None,months=6):
-  fig,ax = plt.subplots(figsize=(12,8))
+def graph_ratio(count_df, title=None):
+  fig, ax = plt.subplots(figsize=(12,8))
   fig.autofmt_xdate()
 
-  n_mon = 12 / months
-  spacing = 365 / n_mon / 2 
-  if months < 2 :
-    months = 2 
-  mon_l = mdates.MonthLocator(interval=months)
+  QE = 3
+  spacing=365/ (12/QE) / 2
+  mon_l = mdates.MonthLocator(bymonth=[1,4,7,10])
   ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
   ax.xaxis.set_major_locator(mon_l)
   
@@ -148,10 +145,11 @@ def graph_ratio(count_df,title=None,months=6):
   ax.set_ylabel('Percentage [%]')
   safe_ratio = count_df['safe_ratio'] 
   quest_ratio = count_df['quest_ratio'] 
+  align='edge'
   ax.bar(x_num,quest_ratio,width=spacing, bottom=safe_ratio,
-        align='center',label='Questionable', color='#F7941D' )
+        align=align, label='Questionable', color='#F7941D' )
   ax.bar(x_num, safe_ratio, width=spacing,
-         align='center',label='Safe',color='#72CDFF')
+         align=align, label='Safe',color='#72CDFF')
 
   ax.annotate("Graph by TecBot with ❤️  ", xy= (0.8,-0.2),
               xycoords='axes fraction', fontsize=10)
@@ -183,9 +181,9 @@ if __name__ == '__main__':
   plt.style.use('ggplot')
   df = process_file(args.file,args.time_start,args.time_stop)
   counts = group_df(df,args.freq)
-  cnt_fig = graph_counts(counts,args.title,months=args.freq)
-  ratio_fig =  graph_ratio(counts,args.title,months=args.freq)
-  lewd_fig =  graph_lewd(counts,args.title)
+  cnt_fig = graph_counts(counts, args.title)
+  ratio_fig =  graph_ratio(counts, args.title)
+  lewd_fig =  graph_lewd(counts, args.title)
 
   #cnt_hd = group_high_def(df)
   #fig_hd = graph_high_def(cnt_hd)
